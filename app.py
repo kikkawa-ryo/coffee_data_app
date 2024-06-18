b9063e51fe4dd35fc91cbaf2a18a6ebe7758847e
ryo_kikkawa <mr.childryo@gmail.com>
2024-06-18 16:26:06 +0900

    データの変更、wordcloudユニーク化、high_bidの集計に変更

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title(emoji.emojize('Coffee Data App:hot_beverage:'))
 st.header("What's this app?")
 url = "https://allianceforcoffeeexcellence.org/competition-auction-results/"
 st.write("[Cup of Excellence](%s) というコーヒーの品評会からデータを収集しています。" % url)
-st.write("streamlitのテストを兼ねて作成しました。")
+st.write("streamlitの勉強を兼ねて作成しました。")
 
 # ダミーデータの作成
 st.subheader('Sample Data')
@@ -25,7 +25,8 @@ from wordcloud import WordCloud
 st.subheader("Word Cloud")
 st.text('風味表現に使われる言葉')
 descriptions = pd.concat([df['acidity_str_agg'], df['aroma_flavor_str_agg'], df['other_str_agg'], df['overall_str_agg'], df['characteristics_str_agg']], ignore_index=True, axis=0).dropna()
-descriptions_list = ",".join(descriptions).split(",")
+descriptions_unique = descriptions.map(lambda l: ",".join(list(set(l.split(",")))) )
+descriptions_list = ",".join(descriptions_unique).split(",")
 c = Counter(descriptions_list)
 d={t[0]: t[1] for t in c.most_common()}
 wordcloud = WordCloud(width=800, height=400, background_color='white').fit_words(d)
@@ -33,25 +34,25 @@ plt.figure()
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 st.pyplot(plt)
-
+# st.write()
 
 # 箱ひげ
 st.subheader("Box Plot")
 st.text('年ごとのスコアの分布')
-chart = alt.Chart(df[['year', "score", "country"]]).mark_boxplot(extent='min-max').encode(
+chart = alt.Chart(df).mark_boxplot(extent='min-max').encode(
         x=alt.X('year:O'),
         y=alt.Y('score:Q', scale=alt.Scale(domain=[80, 100])),
-    )
+    ).interactive()
 st.altair_chart(chart, theme="streamlit", use_container_width=True)
 
 
 # 散布図
 st.subheader("Scatter Plot")
 st.text('スコアとオークション価格の散布図')
-chart = alt.Chart(df[["score", "total_value", "year"]]).mark_circle(size=60).encode(
+chart = alt.Chart(df).mark_circle(size=10).encode(
     x=alt.X('score:Q', scale=alt.Scale(domain=[80, 100])),
-    y=alt.Y('total_value:Q', scale=alt.Scale(domain=[0, 250000])),
-    color=alt.Color('year:Q', scale=alt.Scale(domain=[1999, 2025], scheme='viridis'))
+    y=alt.Y('high_bid:Q', scale=alt.Scale(domain=[0, 500])),
+    color=alt.Color('year:Q', scale=alt.Scale(domain=[1998, 2025], scheme='viridis'))
 ).interactive()
 st.altair_chart(chart, theme="streamlit", use_container_width=True)
 
@@ -59,18 +60,33 @@ st.altair_chart(chart, theme="streamlit", use_container_width=True)
 # 時系列折れ線
 st.subheader("Line Plot")
 st.text('時系列折れ線')
-res3 = df[["country", "year", "total_value"]].dropna().groupby(["country", "year"]).mean().reset_index()
-st.line_chart(
-    data=res3,
-    x="year", y="total_value",
-    color="country",
-    width=1, height=None,
-    # use_container_width=True
+agg_avg = df[["country", "year", "high_bid"]].dropna().groupby(["country", "year"]).mean().reset_index()
+agg_min = df[["country", "year", "high_bid"]].dropna().groupby(["country", "year"]).min().reset_index()
+agg_max = df[["country", "year", "high_bid"]].dropna().groupby(["country", "year"]).max().reset_index()
+avg_chart = alt.Chart(agg_avg).mark_line().encode(
+    x=alt.X('year:Q', scale=alt.Scale(domain=[1998, 2025])),
+    y=alt.Y('high_bid:Q', scale=alt.Scale(domain=[0, 50])).title("Avg. high_bid"),
+    color=alt.Color('country')
+).interactive()
+min_chart = alt.Chart(agg_min).mark_line().encode(
+    x=alt.X('year:Q', scale=alt.Scale(domain=[1998, 2025])),
+    y=alt.Y('high_bid:Q', scale=alt.Scale(domain=[0, 20])).title("Min. high_bid"),
+    color=alt.Color('country')
+).interactive()
+max_chart = alt.Chart(agg_max).mark_line().encode(
+    x=alt.X('year:Q', scale=alt.Scale(domain=[1998, 2025])),
+    y=alt.Y('high_bid:Q', scale=alt.Scale(domain=[0, 450])).title("Max. high_bid"),
+    color=alt.Color('country')
+).interactive()
+chart = alt.VConcatChart(
+        vconcat=(avg_chart, min_chart, max_chart)
+    )
+st.altair_chart(
+    chart,
+    theme="streamlit",
+    use_container_width=True
 )
 
-# st.header("Result-3")
-# res3 = df[["country", "year", "total_value"]].dropna().groupby(["country", "year"]).mean().reset_index()
-# plot = sns.heatmap(df.corr(), annot=True)
-# st.pyplot(plot.get_figure())
+
 
 
