b79ebd35c153d87adec4944023fbbd2505c803cd
ryo_kikkawa <mr.childryo@gmail.com>
2024-10-16 23:02:40 +0900

    dataframeをセッションに保存

--- pages/02_boxplot.py
+++ pages/02_boxplot.py
@@ -10,6 +10,9 @@ from google.cloud import bigquery
 
 from utils.utils import return_national_flag
 
+_="""
+全ページ共通の処理
+"""
 # サイドバー
 with st.sidebar:
     st.page_link("app.py", label="ホーム", icon="🏠")
@@ -20,25 +23,33 @@ with st.sidebar:
     st.page_link("pages/01_wordcloud.py", label="wordcloud", icon="🍷")
     st.page_link("pages/gallery.py", label="gallery", icon="🖼")
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
-sql = """
-    SELECT
-        *
-    FROM
-        `coffee-research`.`coffee_house`.`rpt_streamlit_sample_data`
-    order by
-        year desc, country, score desc
-"""
-df = client.query(sql).to_dataframe()
-st.dataframe(df)
-df['country'] = df['country'].apply(lambda x: x.replace("-"," ").title()).apply(lambda x: return_national_flag(x) + x)
-df = df.convert_dtypes()
-df[['score', 'high_bid', 'total_value', 'weight_lb', 'weight_kg', 'min_altitude', 'avg_altitude', 'max_altitude']] = df[['score', 'high_bid', 'total_value', 'weight_lb', 'weight_kg', 'min_altitude', 'avg_altitude', 'max_altitude']].astype(float)
+if 'df' not in st.session_state:
+    # Create API client.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bigquery.Client(credentials=credentials)
+    sql = """
+        SELECT
+            *
+        FROM
+            `coffee-research`.`coffee_house`.`rpt_streamlit_sample_data`
+        order by
+            year desc, country, score desc
+    """
+    # dataframeの作成
+    df = client.query(sql).to_dataframe()
+    df['country'] = df['country'].apply(lambda x: x.replace("-"," ").title()).apply(lambda x: x +return_national_flag(x))
+    df = df.convert_dtypes()
+    df[['score', 'high_bid', 'total_value', 'weight_lb', 'weight_kg', 'min_altitude', 'avg_altitude', 'max_altitude']] = df[['score', 'high_bid', 'total_value', 'weight_lb', 'weight_kg', 'min_altitude', 'avg_altitude', 'max_altitude']].astype(float)
+    # dataframeをセッションに保存
+    st.session_state.df = df
+df = st.session_state.df
 
+
+_="""
+メイン処理
+"""
 # 箱ひげ
 st.subheader("Box Plot")
 box1, box2 = st.columns(2)
